66e370559de15849484f788fb40a78e7e92a97ed
facusaba <52511760+facusaba@users.noreply.github.com>
2019-07-12 21:59:24 -0300

    parciales (seguir haciendo)

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,35 @@
 function mostrar()
 {
+    var numero1;
+    var numero2;
+    var resultado;
+
+    numero1=prompt(numero1);
+    numero2=prompt(numero2);
+
+    if(numero1==numero2)
+    {
+        resultado=numero1+numero2;
+    }else
+        {
+            if(numero1>numero2)
+            {
+               resultado=numero1-numero2;
+
+            }else
+                {
+                    numero1=parseInt(numero1);
+                    numero2=parseInt(numero2);
+
+                    resultado=numero1+numero2;
+                    
+                    if(resultado>10)
+                    {
+                        alert("la suma es "+resultado+" y es mayor a 10")
+                    }
+                }
+        }
+
+    alert(resultado);
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,18 @@
 
 function mostrar()
 {
+    var ancho;
+    var largo;
+    var perimetro;
 
+    ancho=prompt(ancho);
+    largo=prompt(largo);
+
+    ancho=parseInt(ancho);
+    largo=parseInt(largo);
+
+    perimetro=ancho+largo;
+
+    alert("perimetro es "+perimetro);
+    
 }
